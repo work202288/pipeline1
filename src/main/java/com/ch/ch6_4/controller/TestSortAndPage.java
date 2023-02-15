2bd601a1ce6d04967f86db7f828e55a2be8d432d
work202288 <work202288@gmail.com>
2023-02-16 01:38:13 +0800

    1

--- src/main/java/com/ch/ch6_4/controller/TestSortAndPage.java
+++ src/main/java/com/ch/ch6_4/controller/TestSortAndPage.java
@@ -15,7 +15,7 @@ public class TestSortAndPage {
 	@RequestMapping("/getInformation")
 	@ResponseBody
 	public String get() {
-        return "pipeline Test Good, pipeline Test OK1 le 66688166uuu";
+        return "pipeline Test Good, pipeline Test OK1 le 66688166LaLaLa";
     }
 	
 	@RequestMapping("/save")
